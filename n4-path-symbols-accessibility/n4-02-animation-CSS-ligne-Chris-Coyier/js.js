b47556385d81bfea03ed640aef36c4c8a9bd4b0c
Amélie <120989561+ameliesiquet@users.noreply.github.com>
2024-02-20 15:35:30 +0100

    finish

--- n4-path-symbols-accessibility/n4-02-animation-CSS-ligne-Chris-Coyier/js.js
+++ n4-path-symbols-accessibility/n4-02-animation-CSS-ligne-Chris-Coyier/js.js
@@ -1,1 +1,6 @@
 /* Affichez dans la console la longueur du path */
+document.addEventListener('DOMContentLoaded', function() {
+    const path = document.querySelector('path');
+    const length = path.getTotalLength();
+    console.log(length);
+});
